6f3b217ae5a20c9e5f1c6dd51731aa085c9a8198
Henri Tremblay <henri.tremblay@gmail.com>
2013-06-07 17:07:51 +0200

    Off heap caching

--- app/src/main/java/com/octo/red/happystore/services/CacheManager.java
+++ app/src/main/java/com/octo/red/happystore/services/CacheManager.java
@@ -1,11 +1,16 @@
 package com.octo.red.happystore.services;
 
-import org.springframework.stereotype.Service;
-
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
 
 /**
  * @author Henri Tremblay
@@ -13,7 +18,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @Service
 public class CacheManager {
 
-    private Map<String, Map<Object, Object>> cacheList = new HashMap<String, Map<Object, Object>>();
+    private Map<String, Map<Object, byte[]>> cacheList = new HashMap<String, Map<Object, byte[]>>();
 
     private AtomicLong hit = new AtomicLong(0);
     private AtomicLong success = new AtomicLong(0);
@@ -21,24 +26,27 @@ public class CacheManager {
     @SuppressWarnings("unchecked")
     public synchronized <T> T getFromCache(String cacheKey, Object key) {
         hit.incrementAndGet();
-        Map<Object, Object> cache = cacheList.get(cacheKey);
+        Map<Object, byte[]> cache = cacheList.get(cacheKey);
         if(cache == null) {
             return null;
         }
-        Object o = cache.get(key);
-        if(o != null) {
+        byte[] buffer = cache.get(key);
+        if(buffer != null) {
             success.incrementAndGet();
+            return unmarshall(buffer);
         }
-        return (T) o;
+        return null;
     }
 
     public synchronized void addToCache(String cacheKey, Object key, Object value) {
-        Map<Object, Object> cache = cacheList.get(cacheKey);
+        Map<Object, byte[]> cache = cacheList.get(cacheKey);
         if(cache == null) {
-            cache = new IdentityHashMap<Object, Object>();
+            cache = new IdentityHashMap<Object, byte[]>();
             cacheList.put(cacheKey, cache);
         }
-        cache.put(key, value);
+        // Put the object off-heap to be easy on the GC
+        byte[] buffer = marshall(value);
+        cache.put(key, buffer);
     }
 
     public long getHit() {
@@ -56,5 +64,45 @@ public class CacheManager {
     public synchronized void clearStatistics() {
         hit.set(0);
         success.set(0);
+    }
+
+    private static byte[] marshall(Object o) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream(4096);
+        try {
+            ObjectOutputStream oout = new ObjectOutputStream(out);
+            oout.writeObject(o);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println(out.toByteArray().length);
+        return out.toByteArray();
+    }
+
+    private static <T> T unmarshall(byte[] buffer) {
+        ObjectInputStream in;
+        try {
+            in = new ObjectInputStream(new ByteArrayInputStream(buffer));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            return (T) in.readObject();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
